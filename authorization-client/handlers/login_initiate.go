792c643424d1494cdce17ba423b1ba0568310afb
Paulo Cândido <paulo.candido@g.globo>
2025-04-23 16:25:16 -0300

    refactor: extract function on LoginInitiateHandler

--- authorization-client/handlers/login_initiate.go
+++ authorization-client/handlers/login_initiate.go
@@ -10,8 +10,13 @@ import (
 )
 
 func LoginInitiateHandler(w http.ResponseWriter, r *http.Request) {
-	state := generateRandomString(16)
+	state := generateRandomState(16)
 
+	http.SetCookie(w, stateCookie(state))
+	http.Redirect(w, r, redirectUrl(state), http.StatusTemporaryRedirect)
+}
+
+func redirectUrl(state string) string {
 	query := url.Values{}
 	query.Add("response_type", "code")
 	query.Add("client_id", config.ClientID())
@@ -19,17 +24,18 @@ func LoginInitiateHandler(w http.ResponseWriter, r *http.Request) {
 	query.Add("scope", "read write")
 	query.Add("state", state)
 
-	http.SetCookie(w, &http.Cookie{
+	return config.AuthorizationServerUrl("authorize", true, &query)
+}
+
+func stateCookie(state string) *http.Cookie {
+	return &http.Cookie{
 		Name:   "oauth_state",
 		Value:  state,
 		MaxAge: int((5 * time.Minute).Seconds()),
-		// use Secure and HttpOnly flags for production
-	})
-
-	http.Redirect(w, r, config.AuthorizationServerUrl("authorize", true, &query), http.StatusTemporaryRedirect)
+	}
 }
 
-func generateRandomString(length int) string {
+func generateRandomState(length int) string {
 	bytes := make([]byte, length)
 	_, err := rand.Read(bytes)
 	if err != nil {
